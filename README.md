e7887a1661fa221efc3926fe0a6c0b62d134ca23
JeremyLoh <jeremyloh03@gmail.com>
2020-05-24 00:00:37 +0800

    Add Django-Crispy-Forms

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ A Blog made using Python (3.7.4), Django (3.0.6) and Bootstrap
 1. Bootstrap - https://getbootstrap.com/
 1. Django - https://docs.djangoproject.com/en/3.0/intro/tutorial01/
 1. ColorSpace - https://mycolor.space/
+1. Django-Crispy-Forms - https://django-crispy-forms.readthedocs.io/en/latest/
--- typing_project/typing_project/settings.py
+++ typing_project/typing_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'register',
     'blog',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Update CSS Framework
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
