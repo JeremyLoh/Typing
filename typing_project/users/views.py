8faddbcbb056874c07e319c20e560bb6eed9c861
JeremyLoh <jeremyloh03@gmail.com>
2020-05-24 16:12:21 +0800

    Require login for profile route

--- typing_project/users/views.py
+++ typing_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -18,5 +19,7 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
+# Add decorator, adds functionality to an existing function
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
