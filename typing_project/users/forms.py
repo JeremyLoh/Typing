ff66be40ea04d1897b7e009d493fb0fffc3891aa
JeremyLoh <jeremyloh03@gmail.com>
2020-05-31 16:47:22 +0800

    Add Profile update forms

--- typing_project/users/forms.py
+++ typing_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -14,3 +15,19 @@ class UserRegisterForm(UserCreationForm):
         model = User
         # Fields to be shown in form (in order)
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    # Form used to update User model
+    # ModelForm, to work with specific database model
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']
